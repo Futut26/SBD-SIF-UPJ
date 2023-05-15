b32952ce97e3778fd36c41a07f02c2cd9f11cb1a
Futut26 <putut.budiutomo@student.upj.ac.id>
2023-05-15 23:48:25 +0700

    Menambahkan sub menu week 5-15

--- component.js
+++ component.js
@@ -10,41 +10,73 @@ function loadHeader() {
 }
 
 function loadSidebar() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById('sidebar').innerHTML = this.responseText;
-      }
-    };
-    xhttp.open('GET', 'components/sidebar.html', true);
-    xhttp.send();
-  }
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('sidebar').innerHTML = this.responseText;
+    }
+  };
+  xhttp.open('GET', 'components/sidebar.html', true);
+  xhttp.send();
+}
+
+function loadSidebarWeek_5() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('sidebar-5-7').innerHTML = this.responseText;
+    }
+  };
+  xhttp.open('GET', 'components/menu-week-5-7.html', true);
+  xhttp.send();
+}
+
+function loadSidebarWeek_8() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('sidebar-8-10').innerHTML = this.responseText;
+    }
+  };
+  xhttp.open('GET', 'components/menu-week-8-10.html', true);
+  xhttp.send();
+}
+
+function loadSidebarWeek_11() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('sidebar-11-15').innerHTML = this.responseText;
+    }
+  };
+  xhttp.open('GET', 'components/menu-week-11-15.html', true);
+  xhttp.send();
+}
 
 function loadFooter() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById('footer').innerHTML = this.responseText;
-      }
-    };
-    xhttp.open('GET', 'components/footer.html', true);
-    xhttp.send();
-  }
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('footer').innerHTML = this.responseText;
+    }
+  };
+  xhttp.open('GET', 'components/footer.html', true);
+  xhttp.send();
+}
 
-  function loadContent(file) {
-    // membuat objek XMLHttpRequest
-    let xhttp = new XMLHttpRequest();
+function loadContent(file) {
+  // membuat objek XMLHttpRequest
+  let xhttp = new XMLHttpRequest();
 
-    // menetapkan callback function saat permintaan AJAX selesai
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        // mengambil isi file HTML lain
-        document.getElementById("main").innerHTML = this.responseText;
-      }
-    };
+  // menetapkan callback function saat permintaan AJAX selesai
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // mengambil isi file HTML lain
+      document.getElementById('main').innerHTML = this.responseText;
+    }
+  };
 
-    // membuat permintaan AJAX untuk file HTML lain
-    xhttp.open("GET", file, true);
-    xhttp.send();
-  }
-
+  // membuat permintaan AJAX untuk file HTML lain
+  xhttp.open('GET', file, true);
+  xhttp.send();
+}
